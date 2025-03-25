e99b1b8c2f9f980e987921605af0f61ce153d694
Gabriel Volpini <gabrielvolpini@192.168.0.5>
2025-03-25 14:30:01 -0300

    Feat refacotoring into components

--- src/App.tsx
+++ src/App.tsx
@@ -1,51 +1,14 @@
-import { useState , useEffect} from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Deluxe } from "./componets/Deluxe"
+import "./App.css"
 
-function App() {
-  useEffect(() => {
-
-    const jwt ="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhY2Nlc3NUb2tlbiI6IjYyMjRDNEM3LUE5MjUtNDAyRi1BMTY3LUQ0MkFDMzk1RDVDNCIsImFtb3VudCI6MjYuMDUsImN1c3RvbWVyIjp7ImZpcnN0TmFtZSI6IkphbWVzIiwibGFzdE5hbWUiOiJCb25kIiwiYmlsbGluZ0FkZHJlc3MiOnsiYWRkcmVzcyI6IjIwIHN0cmVldCBhZGRyZXNzIiwiY2l0eSI6IkRhbGxhcyIsInN0YXRlIjoiVFgiLCJ6aXBDb2RlIjoiNTAwNTQiLCJjb3VudHJ5Q29kZSI6IlVTQSJ9fX0.LuZ9WRYK4RLAcHwiHzhG7T-vnlsmWXaNF3ZlEq7-LCU"
-
-    EmbeddedPayments.init(jwt, {
-    countryCode: "US",
-    currencyCode: "USD",
-    paymentMethods: ["ach", "cc"],
-    merchantCapabilities: ["supports3DS"],
-    allowedCardAuthMethods: ["PAN_ONLY", "CRYPTOGRAM_3DS"],
-    supportedNetworks: ["visa", "masterCard", "amex", "discover"],
-    googlePayEnv: "TEST",
-}).then((instance) => {
-    instance
-        .setEventHandlers({
-            onTxnSuccess: (gateway, data) => {
-                console.log(`${gateway} Transaction Succeeded: ${JSON.stringify(data)}`);
-            },
-            onTxnFailed: (gateway, data) => {
-                console.log(`${gateway} Transaction Failed: ${JSON.stringify(data)}`);
-            },
-            onValidationError: (gateway, errors) => {
-                console.log(`Validation Error: ${JSON.stringify(errors)}`);
-            },
-            onCancel: (gateway) => {
-                console.log(`${gateway} transaction cancelled`);
-            }
-        })
-        .render({
-            containerId: "embeddedpayments"
-        });
-});
-  }, [])
-  
-
-  return (
-    <>
-      <div>Hey</div>
-      <div id="embeddedpayments"></div>
-
-    </>
-  )
+export function App() {
+    const jwt = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhY2Nlc3NUb2tlbiI6IjYyMjRDNEM3LUE5MjUtNDAyRi1BMTY3LUQ0MkFDMzk1RDVDNCIsImFtb3VudCI6MjYuMDUsImN1c3RvbWVyIjp7ImZpcnN0TmFtZSI6IkphbWVzIiwibGFzdE5hbWUiOiJCb25kIiwiYmlsbGluZ0FkZHJlc3MiOnsiYWRkcmVzcyI6IjIwIHN0cmVldCBhZGRyZXNzIiwiY2l0eSI6IkRhbGxhcyIsInN0YXRlIjoiVFgiLCJ6aXBDb2RlIjoiNTAwNTQiLCJjb3VudHJ5Q29kZSI6IlVTQSJ9fX0.LuZ9WRYK4RLAcHwiHzhG7T-vnlsmWXaNF3ZlEq7-LCU"
+    return (
+        <div>
+            <div> hey hey</div>
+            <Deluxe jwt={jwt}/>
+        </div>
+    )
 }
 
 export default App
---
+++ src/componets/Deluxe.tsx
@@ -0,0 +1,39 @@
+import { useEffect } from 'react'
+
+export function Deluxe({jwt}) {
+    useEffect(() => {
+        EmbeddedPayments.init(jwt, {
+            countryCode: "US",
+            currencyCode: "USD",
+            paymentMethods: ["ach", "cc"],
+            merchantCapabilities: ["supports3DS"],
+            allowedCardAuthMethods: ["PAN_ONLY", "CRYPTOGRAM_3DS"],
+            supportedNetworks: ["visa", "masterCard", "amex", "discover"],
+            googlePayEnv: "TEST",
+        }).then((instance) => {
+                instance
+                    .setEventHandlers({
+                        onTxnSuccess: (gateway, data) => {
+                            console.log(`${gateway} Transaction Succeeded: ${JSON.stringify(data)}`);
+                        },
+                        onTxnFailed: (gateway, data) => {
+                            console.log(`${gateway} Transaction Failed: ${JSON.stringify(data)}`);
+                        },
+                        onValidationError: (gateway, errors) => {
+                            console.log(`Validation Error: ${JSON.stringify(errors)}`);
+                        },
+                        onCancel: (gateway) => {
+                            console.log(`${gateway} transaction cancelled`);
+                        }
+                    })
+                    .render({
+                        containerId: "embeddedpayments"
+                    });
+            });
+    }, [])
+
+    return (
+        <div id="embeddedpayments"></div>
+    )
+}
+
