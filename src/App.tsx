c57cf99a21b169e871bbec68fdf246d120821c2b
kanishkthapa <kanishk@betalectic.com>
2025-04-14 21:04:21 +0530

    fixed build issues and also added id in the params

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,20 @@
-import { Deluxe } from "./componets/Deluxe"
-import "./App.css"
-import { generateToken } from "./utils/generateToken"
-import { useEffect, useState } from "react"
+import { Deluxe } from "./components/Deluxe";
+import "./App.css";
+import { generateToken } from "./utils/generateToken";
+import { useEffect, useState } from "react";
 
 export function App() {
-    const [jwt, setJwt] = useState<any>(null)
-    useEffect(() => {
-        generateToken().then(setJwt)
-    }, [])
+  const [jwt, setJwt] = useState<string | null>(null);
+  useEffect(() => {
+    generateToken().then(setJwt);
+  }, []);
 
-
-
-    return (
-        <div>
-            <div> hey hey</div>
-            {jwt && <Deluxe jwt={jwt} />}
-        </div>
-    )
+  return (
+    <div>
+      <div>hey hey</div>
+      {jwt && <Deluxe jwt={jwt} />}
+    </div>
+  );
 }
 
-export default App
+export default App;
