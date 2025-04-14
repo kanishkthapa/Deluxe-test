aadb3bf19d1a5550dd005c738b437d6a37874408
kanishkthapa <kanishk@betalectic.com>
2025-04-14 22:04:19 +0530

    fixed the error

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App'
 
-createRoot(document.getElementById('root')!).render(
+const rootElement = document.getElementById("root");
+if (!rootElement) throw new Error("Failed to find the root element");
+const root = createRoot(rootElement);
+
+root.render(
   <StrictMode>
-    <App/>
-  </StrictMode>,
-)
+    <App />
+  </StrictMode>
+);
