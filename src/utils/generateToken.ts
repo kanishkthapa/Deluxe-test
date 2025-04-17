2b60bdca0bbb402f9c043eb58a37fe73fa4ae02a
kanishkthapa <kanishk@betalectic.com>
2025-04-17 13:11:29 +0530

    re-added recurring info

--- src/utils/generateToken.ts
+++ src/utils/generateToken.ts
@@ -9,23 +9,23 @@ export async function generateToken() {
     accessToken: import.meta.env.VITE_ACCESS_TOKEN,
     amount: 2,
     transactionReference: transactionId,
-    // hideCancelButton: true,
-    // hideaddresspanel: true,
-    // hidetermsandconditions: true,
-    // hideGooglePayButton: true,
-    // hideApplePayButton: true,
-    // recurring: {
-    //   frequency: "daily",
-    //   startDate: new Date().toISOString().split("T")[0],
-    //   endDate: new Date(new Date().setDate(new Date().getDate() + 5))
-    //     .toISOString()
-    //     .split("T")[0],
-    //   description: "Auto Renewal for the Quote",
-    //   managementUrl: "https://test.com",
-    //   billingAgreement:
-    //     "I agree to the terms and conditions from test company.",
-    //   intervals: 5,
-    // },
+    hideCancelButton: true,
+    hideaddresspanel: true,
+    hidetermsandconditions: true,
+    hideGooglePayButton: true,
+    hideApplePayButton: true,
+    recurring: {
+      frequency: "daily",
+      startDate: new Date().toISOString().split("T")[0],
+      endDate: new Date(new Date().setDate(new Date().getDate() + 5))
+        .toISOString()
+        .split("T")[0],
+      description: "Auto Renewal for the Quote",
+      managementUrl: "https://test.com",
+      billingAgreement:
+        "I agree to the terms and conditions from test company.",
+      intervals: 5,
+    },
   })
     .setProtectedHeader({ alg: "HS256" })
     .sign(secret);
