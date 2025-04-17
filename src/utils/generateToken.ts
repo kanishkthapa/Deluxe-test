420b79659b13116e216da93c5a4a9a46ace668af
kanishkthapa <kanishk@betalectic.com>
2025-04-17 13:01:17 +0530

    removed recurring

--- src/utils/generateToken.ts
+++ src/utils/generateToken.ts
@@ -7,25 +7,25 @@ export async function generateToken() {
   const secret = new TextEncoder().encode(import.meta.env.VITE_JWT_SECRET);
   const jwt = await new SignJWT({
     accessToken: import.meta.env.VITE_ACCESS_TOKEN,
-    amount: 1.05,
+    amount: 2,
     transactionReference: transactionId,
     // hideCancelButton: true,
     // hideaddresspanel: true,
     // hidetermsandconditions: true,
     // hideGooglePayButton: true,
     // hideApplePayButton: true,
-    recurring: {
-      frequency: "daily",
-      startDate: new Date().toISOString().split("T")[0],
-      endDate: new Date(new Date().setDate(new Date().getDate() + 5))
-        .toISOString()
-        .split("T")[0],
-      description: "Auto Renewal for the Quote",
-      managementUrl: "https://test.com",
-      billingAgreement:
-        "I agree to the terms and conditions from test company.",
-      intervals: 5,
-    },
+    // recurring: {
+    //   frequency: "daily",
+    //   startDate: new Date().toISOString().split("T")[0],
+    //   endDate: new Date(new Date().setDate(new Date().getDate() + 5))
+    //     .toISOString()
+    //     .split("T")[0],
+    //   description: "Auto Renewal for the Quote",
+    //   managementUrl: "https://test.com",
+    //   billingAgreement:
+    //     "I agree to the terms and conditions from test company.",
+    //   intervals: 5,
+    // },
   })
     .setProtectedHeader({ alg: "HS256" })
     .sign(secret);
