22f19cbf6e511ee2ef3c6633042ee7c1a1473505
kanishkthapa <kanishk@betalectic.com>
2025-04-17 16:08:59 +0530

    using axios

--- src/utils/fetchPurchase.ts
+++ src/utils/fetchPurchase.ts
@@ -1,30 +1,26 @@
-// import axios from "axios";
-
+import axios from "axios";
 
 async function fetchPurchase(): Promise<string | undefined> {
-  try {
-    const urlParams = new URLSearchParams(window.location.search);
-    const transactionId = urlParams.get("id") || "default-transaction";
+  const urlParams = new URLSearchParams(window.location.search);
+  const transactionId = urlParams.get("id") || "default-transaction";
 
-    console.log("transactionId", transactionId);
-    console.log("VITE_POLICY_API_URL", import.meta.env.VITE_POLICY_API_URL);
-    console.log("VITE_API_KEY", import.meta.env.VITE_API_KEY);
-    console.log("VITE_JWT_SECRET", import.meta.env.VITE_JWT_SECRET);
-    const response = await fetch(
-      `${import.meta.env.VITE_POLICY_API_URL}/purchases/${transactionId}`,
-      {
-        method: "GET",
-        headers: {
-          Authorization: `Bearer ${import.meta.env.VITE_API_KEY}`,
-        },
-      }
-    );
-    if (!response.ok) {
-      throw new Error(`HTTP error! status: ${response.status}`);
-    }
-    const data = await response.json();
-    console.log("response from fetchPurchase", JSON.stringify(data));
-    return data?.payment_gateway?.embedded_payment_config?.jwt;
+  console.log("transactionId", transactionId);
+  console.log("VITE_POLICY_API_URL", import.meta.env.VITE_POLICY_API_URL);
+  console.log("VITE_API_KEY", import.meta.env.VITE_API_KEY);
+
+  const config = {
+    method: "get",
+    maxBodyLength: Infinity,
+    url: `${import.meta.env.VITE_POLICY_API_URL}/purchases/${transactionId}`,
+    headers: {
+      Authorization: `Bearer ${import.meta.env.VITE_API_KEY}`,
+    },
+  };
+
+  try {
+    const response = await axios.request(config);
+    console.log("response from fetchPurchase", JSON.stringify(response.data));
+    return response.data?.payment_gateway?.embedded_payment_config?.jwt;
   } catch (error) {
     console.error("Error fetching purchase:", error);
     return undefined;
